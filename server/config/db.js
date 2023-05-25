b0786f4547da9ee54e08a3239931d3a4faf237a7
aftab-alam-khan <aftab.khan@pager.com>
2023-05-25 19:31:44 -0400

    add Client page

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/mgmt_db';
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(MONGO_URL);
+    try{
+        const conn = await mongoose.connect(MONGO_URL);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    mongoose.connection.on('disconnected', () => console.log(`db Connection is closed`));
+    mongoose.connection.on('error', (error) => console.log(`Connection disconnected due to some error: `, error.name));
+    }
+    catch(err) {
+        console.log(`Connection disconnected due to some error: `, err.message);
+    }
 };
 
 module.exports = connectDB;
